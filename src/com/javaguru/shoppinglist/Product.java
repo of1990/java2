f510c94c9c583e5044a7633ae8538160a271aa53
olegsfjodorovs <fjodorovsoa@gmail.com>
2020-01-27 22:55:15 +0200

    initial project structure

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -1,12 +1,44 @@
 package com.javaguru.shoppinglist;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Product {
 
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private BigDecimal discount;
+    private String description;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) throws Exception {
+        if (discount.intValue() < 100) {
+            this.discount = discount.setScale(2,RoundingMode.HALF_EVEN);
+        } else {
+            throw new Exception("The discount cannot be more than 100");
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Long getId() {
         return id;
@@ -20,16 +52,35 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) throws Exception {
+        if (name.length() > 3 && name.length() < 32) {
+            this.name = name;
+        } else {
+            throw new Exception ("The name must be no shorter than 3 characters and no longer than 32 characters");
+        }
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    public void setPrice(BigDecimal price) throws Exception {
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new Exception("Price must be greater than 0");
+        } else {
+            this.price = price.setScale(2,RoundingMode.HALF_EVEN);
+        }
     }
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -15,31 +15,75 @@ class ShoppingListApplication {
             try {
                 System.out.println("1. Create product");
                 System.out.println("2. Find product by id");
-                System.out.println("3. Exit");
+                System.out.println("3. Update product");
+                System.out.println("4. Delete product");
+                System.out.println("5. Exit");
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
+                        Product product = new Product();
                         System.out.println("Enter product name: ");
                         String name = scanner.nextLine();
+                        product.setName(name);
                         System.out.println("Enter product price: ");
                         BigDecimal price = new BigDecimal(scanner.nextLine());
-                        Product product = new Product();
-                        product.setName(name);
                         product.setPrice(price);
+                        System.out.println("Enter product category: ");
+                        String category = scanner.nextLine();
+                        product.setCategory(category);
+                        System.out.println("Enter product discount");
+                        BigDecimal discount = new BigDecimal(scanner.nextLine());
+                        product.setDiscount(discount);
+                        System.out.println("Enter product description");
+                        String description = scanner.nextLine();
+                        product.setDescription(description);
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
-                        System.out.println("Result: " + product.getId());
+                        System.out.println("Product ID:" + product.getId());
+                        break;
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
                         System.out.println(findProductResult);
+                        break;
                     case 3:
+                        System.out.println("Enter product id: ");
+                        id = scanner.nextLong();
+                        findProductResult = productRepository.get(id);
+                        System.out.println("Enter product name: ");
+                        name = scanner.next();
+                        findProductResult.setName(name);
+                        System.out.println("Enter product price: ");
+                        price = new BigDecimal(scanner.next());
+                        findProductResult.setPrice(price);
+                        System.out.println("Enter product category: ");
+                        category = scanner.next();
+                        findProductResult.setCategory(category);
+                        System.out.println("Enter product discount");
+                        discount = new BigDecimal(scanner.next());
+                        findProductResult.setDiscount(discount);
+                        System.out.println("Enter product description");
+                        description = scanner.next();
+                        findProductResult.setDescription(description);
+                        break;
+                    case 4:
+                        System.out.println("Enter product id: ");
+                        id = scanner.nextLong();
+                        if (productRepository.containsKey(id)) {
+                            productRepository.remove(id);
+                            System.out.println("Product has been removed!");
+                        } else {
+                            throw new Exception("id not found or entered incorrectly");
+                        }
+                        break;
+
+                    case 5:
                         return;
                 }
             } catch (Exception e) {
-                System.out.println("Error! Please try again.");
+                System.out.println("Error! Please try again. " + e.getMessage());
             }
         }
     }
