8dab3c71139fe30b700b05c4da56959f9599ecf5
olegsfjodorovs <fjodorovsoa@gmail.com>
2020-02-17 21:48:07 +0200

    tests

--- src/test/java/service/validation/ValidationServiceTest.java
+++ src/test/java/service/validation/ValidationServiceTest.java
@@ -21,48 +21,110 @@ public class ValidationServiceTest {
     private ProductRepository repository;
     @InjectMocks
     private ValidationService victim;
-    private Product product = product();
 
-    private Product product() {
-        Product product = new Product();
-        product.setName("apple");
-        product.setDiscount(new BigDecimal(35));
-        product.setPrice(new BigDecimal(40));
-        product.setCategory("fruits");
-        product.setDescription("tasty");
-        product.setId(1L);
-        return product;
-    }
 
 
     @Test
-    public void validateUniqueProductName() {
+    public void shouldЕhrowExceptionIfNameIsNotUnique() {
+        Product product = new Product();
+        product.setName("apple");
+        product.setDiscount(new BigDecimal(25));
+        product.setPrice(new BigDecimal(3));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
         when(repository.findByName(product.getName())).thenReturn(product);
         assertThatThrownBy(() -> victim.validateUniqueProductName(product)).
                 isInstanceOf(ProductValidationException.class).hasMessage("Product with this name already exists!");
     }
 
     @Test
-    public void validateProduct() {
-        when(repository.addProduct(product)).thenReturn(product);
+    public void shouldReturnProductIfNameIsUnique() {
+
+    }
+
+    
+    @Test
+    public void shouldThrowExceptionNameMustNotBeLessThan3() {
+        Product product = new Product();
+        product.setName("a");
+        product.setDiscount(new BigDecimal(25));
+        product.setPrice(new BigDecimal(3));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
         assertThatThrownBy(() -> victim.validateProduct(product)).
                 isInstanceOf(ProductValidationException.class).
                 hasMessage("The name must be no shorter than 3 characters and no longer than 32 characters");
-        assertThatThrownBy(() -> victim.validateProduct(product)).
-                isInstanceOf(ProductValidationException.class).
-                hasMessage("Price must be greater than 0");
-        assertThatThrownBy(() -> victim.validateProduct(product)).
-                isInstanceOf(ProductValidationException.class).
-                hasMessage("The discount cannot be less than 0 and more than 100");
-
 
     }
 
     @Test
-    public void validateId() {
-
+    public void shouldThrowExceptionNameMustNotBeMoreThan32() {
+        Product product = new Product();
+        product.setName("asdfdsfkeeefjjfkkekkdkkdkdkkekkdsdsdfkkwfrfrferfef");
+        product.setDiscount(new BigDecimal(25));
+        product.setPrice(new BigDecimal(3));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
+        assertThatThrownBy(() -> victim.validateProduct(product)).
+                isInstanceOf(ProductValidationException.class).
+                hasMessage("The name must be no shorter than 3 characters and no longer than 32 characters");
 
     }
+
+    @Test
+    public void shouldThrowExceptionPriceMustBeGreaterThan0() {
+        Product product = new Product();
+        product.setName("apple");
+        product.setDiscount(new BigDecimal(25));
+        product.setPrice(new BigDecimal(-1));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
+        assertThatThrownBy(() -> victim.validateProduct(product)).
+                isInstanceOf(ProductValidationException.class).
+                hasMessage("Price must be greater than 0");
+    }
+
+    @Test
+    public void shouldThrowExceptionPDiscountCannotBeLessThan0() {
+        Product product = new Product();
+        product.setName("apple");
+        product.setDiscount(new BigDecimal(-1));
+        product.setPrice(new BigDecimal(15));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
+        assertThatThrownBy(() -> victim.validateProduct(product)).
+                isInstanceOf(ProductValidationException.class).
+                hasMessage("The discount cannot be less than 0 and more than 100");
+    }
+
+    @Test
+    public void shouldThrowExceptionPDiscountCannotBeMoreThan100() {
+        Product product = new Product();
+        product.setName("apple");
+        product.setDiscount(new BigDecimal(120));
+        product.setPrice(new BigDecimal(15));
+        product.setCategory("fruits");
+        product.setDescription("tasty");
+        product.setId(1L);
+        assertThatThrownBy(() -> victim.validateProduct(product)).
+                isInstanceOf(ProductValidationException.class).
+                hasMessage("The discount cannot be less than 0 and more than 100");
+    }
+
+    @Test
+    public void validateProductById() {
+        Product product = null;
+        assertThatThrownBy(() -> victim.validateId(product)).
+                isInstanceOf(ProductValidationException.class).
+                hasMessage("Id not found or entered incorrectly");
+
+    }
+
 
 
 }
