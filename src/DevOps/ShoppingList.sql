e79d8ac0559bbbbbe5ed44539a05e2c36d93a126
olegsfjodorovs <fjodorovsoa@gmail.com>
2020-04-19 21:06:03 +0300

    ShoppingCart

--- src/DevOps/ShoppingList.sql
+++ src/DevOps/ShoppingList.sql
@@ -1,10 +1,24 @@
-CREATE TABLE `product` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `price` decimal(10,0) DEFAULT NULL,
-  `category` varchar(100) DEFAULT NULL,
-  `discount` decimal(10,0) DEFAULT NULL,
-  `description` varchar(300) DEFAULT NULL,
-  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE IF NOT EXISTS product
+(
+    id          BIGINT      NOT NULL AUTO_INCREMENT,
+    shopping_cart_id BIGINT,
+    name        VARCHAR(32) NOT NULL,
+    price       DECIMAL,
+    category    VARCHAR(100),
+    discount    DECIMAL,
+    description VARCHAR(300),
+    created     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (shopping_cart_id) REFERENCES shoppingCart(id),
+    PRIMARY KEY (id)
+
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 1;
+
+CREATE TABLE IF NOT EXISTS shoppingCart
+(
+    id   BIGINT      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
